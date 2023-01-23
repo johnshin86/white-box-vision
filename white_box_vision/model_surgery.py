8b05e5031f2e2e6192d0b6a2e8fe5f3f0b13afe3
John Shin <johnyoungshin@gmail.com>
2023-01-23 16:51:01 -0500

    Working on residual stream.

--- white_box_vision/model_surgery.py
+++ white_box_vision/model_surgery.py
@@ -49,7 +49,7 @@ def get_model_layers(model: torch.nn.Module) -> tuple[str, torch.nn.ModuleList]:
 	"""Get model layers from a model. Need to think about how to best handle this
 	for vision models.
 	"""
-	return None
+	pass
 
 
 # Functions to manipulate a layer
--- white_box_vision/residual_stream.py
+++ white_box_vision/residual_stream.py
@@ -1,5 +1,33 @@
 import torch
+import torch.distributed as dist
+
+from contextlib import contextmanager
+from dataclasses import dataclass, field 
+from itertools import starmap, zip_longest
+from typing import Callable, Generator, Iterable, Optional, overload, Type, Union
+
+from .model_surgery import get_transformer_layers
+
+
 
 
 # functions that generate and store intermediate representations
 # of a vision model. 
+
+
+@dataclass
+class ResidualStream:
+
+	embeddings: Optional[torch.Tensor] = None
+	attentions: list[torch.Tensor] = field(default_factory = list)
+	layers: list[torch.Tensor] = field(default_factory = list)
+
+
+@contextmanager
+def record_residual_stream(
+	model: torch.nn.Module,
+	*,
+	
+
+	)
+
