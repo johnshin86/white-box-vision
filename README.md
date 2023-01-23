aa7143ac8d22740d99320d8cf7465e6da7100296
John Shin <johnyoungshin@gmail.com>
2023-01-23 15:51:42 -0500

    Cleaning up comments.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # white-box-vision
 
-The goal of the white box vision libraries is to test vision models for interpretability.
+The goal of the white box vision library is to test vision models for interpretability, attribution, and visualization. The goal of the library is to enable interactive exploration of a pre-trained model in a notebook, and conduct ablation experiments on intermediate representations and how a model builds its prediction. 
 
 
-This library was heavily inspired by the work of Nora Belrose's whitebox library (https://github.com/norabelrose/white-box), but has been re-built from the ground up for vision models. 
+This library was based on Nora Belrose's whitebox library (https://github.com/norabelrose/white-box), but has been re-built from the ground up for vision models. 
--- white_box_vision/model_surgery.py
+++ white_box_vision/model_surgery.py
@@ -1,32 +1,13 @@
 import torch
 import timm
+from contextlib import contextmanager
 from typing import Any, Generator, Optional, Type, TypeVar, Union
 
 
 
-from timm.models.vision_transformer import Block 
 
 # pretrained models will be from huggingface, 
 # since afaik, it's the most popular NN arch library. 
-
-# Suite of functions that will add / remove / replace / retrieve layers
-# from a vision model.
-
-# We want to be able to retrieve attention masks from the vision transformer. 
-# As well as intermediate representations of the token(s). 
-
-# We can either monkey patch the attention and block modules
-# to return attention, or we can create subclasses of attention
-# and block, which override the method. 
-
-# Accessing specific layers in a pytorch model is a little wonky
-# since it requires typing attributes in a chain. Sometimes
-# the attributes are numbers, and sometimes they are strings.
-
-#Helper functions to retrieve NN layers using strings. 
-
-#Generator[yield_type, send_type, return_type]
-
 
 # Functions to retrieve layers with strings
 
