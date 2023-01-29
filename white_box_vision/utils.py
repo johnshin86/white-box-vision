73724cdbcb9cef983d76de7ad319b245acbe2fc4
John Shin <johnyoungshin@gmail.com>
2023-01-29 14:31:40 -0500

    working on utils

---
+++ white_box_vision/utils.py
@@ -0,0 +1,8 @@
+from itertools import islice
+from typing import Any, Callable, Iterable, Sequence, TypeVar, Union
+import torch as torch
+import torch.distributed as dist
+
+def maybe_all_cat(x: torch.Tensor) -> torch.Tensor:
+	pass
+
